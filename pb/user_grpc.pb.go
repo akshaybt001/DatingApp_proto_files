be6b662a8dfd3175274df5d4c6989d6a99843a92
Akshay Babu <akshaybabut1@gmail.com>
2024-05-05 16:56:30 +0530

    is user exist response small change

--- pb/user_grpc.pb.go
+++ pb/user_grpc.pb.go
@@ -48,6 +48,7 @@ const (
 	UserService_UserUploadProfileImage_FullMethodName = "/user.UserService/UserUploadProfileImage"
 	UserService_UserGetProfilePic_FullMethodName      = "/user.UserService/UserGetProfilePic"
 	UserService_HomePage_FullMethodName               = "/user.UserService/HomePage"
+	UserService_IsUserExist_FullMethodName            = "/user.UserService/IsUserExist"
 )
 
 // UserServiceClient is the client API for UserService service.
@@ -83,6 +84,7 @@ type UserServiceClient interface {
 	UserUploadProfileImage(ctx context.Context, in *UserImageRequest, opts ...grpc.CallOption) (*UserImageResponse, error)
 	UserGetProfilePic(ctx context.Context, in *GetUserById, opts ...grpc.CallOption) (*UserImageResponse, error)
 	HomePage(ctx context.Context, in *GetUserById, opts ...grpc.CallOption) (*HomeResponse, error)
+	IsUserExist(ctx context.Context, in *GetUserById, opts ...grpc.CallOption) (*IsUserExistResponse, error)
 }
 
 type userServiceClient struct {
@@ -423,6 +425,15 @@ func (c *userServiceClient) HomePage(ctx context.Context, in *GetUserById, opts 
 	return out, nil
 }
 
+func (c *userServiceClient) IsUserExist(ctx context.Context, in *GetUserById, opts ...grpc.CallOption) (*IsUserExistResponse, error) {
+	out := new(IsUserExistResponse)
+	err := c.cc.Invoke(ctx, UserService_IsUserExist_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility
@@ -456,6 +467,7 @@ type UserServiceServer interface {
 	UserUploadProfileImage(context.Context, *UserImageRequest) (*UserImageResponse, error)
 	UserGetProfilePic(context.Context, *GetUserById) (*UserImageResponse, error)
 	HomePage(context.Context, *GetUserById) (*HomeResponse, error)
+	IsUserExist(context.Context, *GetUserById) (*IsUserExistResponse, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -549,6 +561,9 @@ func (UnimplementedUserServiceServer) UserGetProfilePic(context.Context, *GetUse
 }
 func (UnimplementedUserServiceServer) HomePage(context.Context, *GetUserById) (*HomeResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method HomePage not implemented")
+}
+func (UnimplementedUserServiceServer) IsUserExist(context.Context, *GetUserById) (*IsUserExistResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsUserExist not implemented")
 }
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 
@@ -1094,6 +1109,24 @@ func _UserService_HomePage_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_IsUserExist_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetUserById)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).IsUserExist(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: UserService_IsUserExist_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).IsUserExist(ctx, req.(*GetUserById))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -1204,6 +1237,10 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "HomePage",
 			Handler:    _UserService_HomePage_Handler,
+		},
+		{
+			MethodName: "IsUserExist",
+			Handler:    _UserService_IsUserExist_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
